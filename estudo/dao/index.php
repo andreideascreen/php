b03407ac4e2a08475f0ec6781ea82df5e881cb1a
andreideascreen <alpmdp@gmail.com>
2018-11-27 20:11:30 -0200

    Atrualização

--- estudo/dao/index.php
+++ estudo/dao/index.php
@@ -1,10 +1,14 @@
 <?php
 require_once("config.php");
 
-$sql = new Sql();
+/*$sql = new Sql();
 
 $usuarios = $sql->select("SELECT *FROM tb_usuarios");
 
-echo json_encode($usuarios);
+echo json_encode($usuarios);*/
+
+$user2 = new Usuario();
+$user2->loadbyID(3);
+echo $user2
 
 ?>
