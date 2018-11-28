0d999cf6cd4cb6eeb80d9942fb95f19edaa4ea3c
andreideascreen <alpmdp@gmail.com>
2018-11-27 23:11:38 -0200

    Atualização DAO Insert

--- estudo/dao/index.php
+++ estudo/dao/index.php
@@ -1,14 +1,27 @@
 <?php
 require_once("config.php");
 
-/*$sql = new Sql();
+// $sql = new Sql();
+// $usuarios = $sql->select("SELECT *FROM tb_usuarios");
+// echo json_encode($usuarios);
 
-$usuarios = $sql->select("SELECT *FROM tb_usuarios");
 
-echo json_encode($usuarios);*/
+//Carrega somente um usuario
+// $user2 = new Usuario();
+// $user2->loadbyID(3);
+// echo $user2
 
-$user2 = new Usuario();
-$user2->loadbyID(3);
-echo $user2
+//Carrega uma lista de usuarios
+// $lista = Usuario::getList();
+// echo json_encode($lista);
+
+//Carrega uma lista de usuarios buscando pelo login
+// $search = Usuario::search("user");
+// echo json_encode($search);
+
+//Carrega um usuário usando o login e senha
+$usuario = new Usuario();
+$usuario->login("user2","123456");
+echo $usuario;
 
 ?>
